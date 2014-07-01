08367f270be6c8ab2e5e00e6baedf77e1e659b00
thalescm <thales.mchd@gmail.com>
2014-07-01 09:24:22 -0300

    initial commit

---
+++ server/models/user.js
@@ -0,0 +1,139 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    crypto = require('crypto');
+ 
+/**
+  * Validations
+  */
+ var validatePresenceOf = function(value) { 
+     // If you are authenticating by any of the oauth strategies, don't validate.
+     return (this.provider && this.provider !== 'local') || (value && value.length);
+ };
+
+/**
+ * User Schema
+ */
+var UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        // match: [/.+\@.+\..+/, 'Please enter a valid email'],
+        match: ['(?:[a-z0-9!#$%\\&\'*+/=?\\^_`{|}~-]+(?:\\.[a-z0-9!#$%\\&\'*+/=?\\^_`{|}~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])' , 'Please enter a valid email'],
+    },
+    username: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    roles: {
+        type: Array,
+        default: ['authenticated']
+    },
+    hashed_password: {
+        type: String,
+        validate: [validatePresenceOf, 'Password cannot be blank']
+    },
+    provider: {
+        type: String,
+        default: 'local'
+    },
+    salt: String,
+    facebook: {},
+    twitter: {},
+    github: {},
+    google: {},
+    linkedin: {}
+});
+
+/**
+ * Virtuals
+ */
+UserSchema.virtual('password').set(function(password) {
+    this._password = password;
+    this.salt = this.makeSalt();
+    this.hashed_password = this.hashPassword(password);
+}).get(function() {
+    return this._password;
+});
+
+/**
+ * Pre-save hook
+ */
+UserSchema.pre('save', function(next) {
+    if (this.isNew && this.provider === 'local' && this.password && !this.password.length)
+        return next(new Error('Invalid password'));
+    next();
+});
+
+/**
+ * Methods
+ */
+UserSchema.methods = {
+
+    /**
+     * HasRole - check if the user has required role
+     *
+     * @param {String} plainText
+     * @return {Boolean}
+     * @api public
+     */
+    hasRole: function(role) {
+        var roles = this.roles;
+        return roles.indexOf('admin') !== -1 || roles.indexOf(role) !== -1;
+    },
+
+    /**
+     * IsAdmin - check if the user is an administrator
+     *
+     * @return {Boolean}
+     * @api public
+     */
+    isAdmin: function() {
+        return this.roles.indexOf('admin') !== -1;
+    },
+
+    /**
+     * Authenticate - check if the passwords are the same
+     *
+     * @param {String} plainText
+     * @return {Boolean}
+     * @api public
+     */
+    authenticate: function(plainText) {
+        return this.hashPassword(plainText) === this.hashed_password;
+    },
+
+    /**
+     * Make salt
+     *
+     * @return {String}
+     * @api public
+     */
+    makeSalt: function() {
+        return crypto.randomBytes(16).toString('base64');
+    },
+
+    /**
+     * Hash password
+     *
+     * @param {String} password
+     * @return {String}
+     * @api public
+     */
+    hashPassword: function(password) {
+        if (!password || !this.salt) return '';
+        var salt = new Buffer(this.salt, 'base64');
+        return crypto.pbkdf2Sync(password, salt, 10000, 64).toString('base64');
+    }
+};
+
+mongoose.model('User', UserSchema);
